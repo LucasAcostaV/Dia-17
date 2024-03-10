1dfb08c4d564a018075e05b90e9acfb9b994d2a8
Lucas Acosta <lucasfabrizzio67@gmail.com>
2024-03-10 12:32:40 -0300

    inicializacion de proyecto

---
+++ src/main/java/com/example/recomendacion/RecomendacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.recomendacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendacionApplication.class, args);
+	}
+
+}
